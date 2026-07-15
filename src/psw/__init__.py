2cc3981b8f05db558e33e7f2734614a8cf1ac2a5
Kenji Otsuka <kok.fdcm@gmail.com>
2026-07-15 20:57:18 +0900

    version 0.1.1

--- src/psw/__init__.py
+++ src/psw/__init__.py
@@ -4,7 +4,7 @@ from psw.stopwatch import Stopwatch
 from psw.timer import Timer
 
 # パッケージのバージョン（pyproject.toml と合わせておくと管理しやすいです）
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # `from psw import *` をした際にインポートされるクラスを指定
 __all__ = ["Stopwatch", "Timer"]
